6d5b11f5b88845ef6148efab41639d7717a01855
Алимбек <balimbek2006@mail.ru>
2025-07-17 17:19:56 +0300

    ready desktop

--- info-actions/site/coral.ru/js/main.js
+++ info-actions/site/coral.ru/js/main.js
@@ -1,6 +1,31 @@
-import {hostReactAppReady, vimeoAutoPlay} from "../../common/js/usefuls.js";
+import {hostReactAppReady} from "../../common/js/usefuls.js";
 
 (async () => {
   await hostReactAppReady();
   
-})()
+  const buttons = document.querySelectorAll('.switch-container-button');
+  const place1 = document.querySelector('.place1');
+  const place2 = document.querySelector('.place2');
+
+  if (buttons.length > 0) {
+    buttons[0].classList.add('active');
+    place1.style.display = 'flex';
+    place2.style.display = 'none';
+  }
+
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('active'));
+      
+      button.classList.add('active');
+
+      if (index === 0) {
+        place1.style.display = 'flex';
+        place2.style.display = 'none';
+      } else {
+        place1.style.display = 'none';
+        place2.style.display = 'flex';
+      }
+    });
+  });
+})();
